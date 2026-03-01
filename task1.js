6fd85fee97d2db45d948ed55ec42f4b580388981
Mamun123123123 <almalm90459@gmail.com>
2026-03-01 19:08:08 +0600

    task1 Done

---
+++ task1.js
@@ -0,0 +1,6 @@
+let btn = document.querySelector(".btn");
+let box = document.querySelector("#box")
+btn.addEventListener("click",()=>{
+    box.style.backgroundColor = "yellow";
+    box.style.color = "black"
+})
