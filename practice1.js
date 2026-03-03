3e00d3f6397b903a0e1671cfde30915b53305065
Mamun123123123 <almalm90459@gmail.com>
2026-03-03 19:59:09 +0600

    basic check

---
+++ practice1.js
@@ -0,0 +1,65 @@
+let a = 10
+let b = 20
+let c = "30"
+//console.log(a+b);
+//console.log(typeof(a+b));//number
+//console.log(a+c);
+//console.log(typeof(a+c));//string
+
+//console.log(`sum of ${a} and ${b} is : ` + a + b);//a ,b operand,, string
+//console.log(`sum of ${a} and ${b} is : ` + (a + b));//a ,b operand,, number
+
+//console.log("1"+1); // type corsion,+ make addition/concatenate
+//console.log("1" - 1);  //- only subtract
+
+//let age = prompt("what is your age")//prompt a sob string
+//console.log(age);
+//let ages = Number(prompt("what is your age"))// this time value is number 
+
+//swap1
+let x = 10;
+let y = 20
+//let z = x
+//x=y
+//y=z
+//console.log(x);
+//console.log(y);
+
+
+//swap2
+a = a+b
+b = a-b
+a = a-b
+
+//swap3
+//[x,y] = [y,x]
+
+console.log(Math.floor(13/5)); // divide always vagfal dibe
+console.log(Math.floor(13%5)); //vagsesh dibe % chinho
+
+//unary operator
+let i = 11
+i = i++ + ++i
+console.log(i);
+
+let u = 11,e=22
+let l = u + e + u++ + e++ + ++u + ++e
+console.log(u);
+console.log(e);
+console.log(l);
+
+let w = true
+w++
+console.log(w);
+
+
+
+
+
+
+
+
+
+
+
+
