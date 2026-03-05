c353376dbf6fc18f7fbb14f854b192ff85b3a358
Mamun123123123 <almalm90459@gmail.com>
2026-03-05 16:20:14 +0600

    second max value and second min value

--- arr.js
+++ arr.js
@@ -42,3 +42,35 @@ for(let i=1;i<min_value.length;i++){
 console.log(min);
 
 
+let ar = [20,30,10,40,90,80,30,200,400]
+let first_max = Math.max(ar[0],ar[1])
+let second_max = Math.min(ar[0],ar[1])
+for(let i = 2;i<ar.length;i++){
+    if(first_max<ar[i]){
+        second_max = first_max
+        first_max = ar[i]   
+    }else if(second_max<ar[i]){
+        second_max = ar[i]
+    }
+}
+console.log(second_max);
+
+let arrr = [40,50,30,90,100,500,800]
+let first_min = Math.min(arrr[0],arrr[1])
+let second_min = Math.max(arrr[0],arrr[1])
+for(let i=2;i<arrr.length;i++){
+    if(first_min>arrr[i]){
+        second_min=first_min
+        first_min=arrr[i]
+    }else if(second_min>arrr[i]){
+        second_min = arrr[i]
+    }
+}
+console.log(first_min);
+
+console.log(second_min);
+
+
+
+
+
