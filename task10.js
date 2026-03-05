563f1313cc822c57ee78d6b5ef210715aa7dd152
Mamun123123123 <almalm90459@gmail.com>
2026-03-05 13:56:09 +0600

    add task,edit and delete operation

--- task10.js
+++ task10.js
@@ -1,33 +1,72 @@
 let tasks = [];
+let editIndex = -1;
 
 function show(){
-    let ul = document.querySelector("#show")
-    ul.innerHTML = ""
+
+    let ul = document.querySelector("#show");
+
+    ul.innerHTML = "";
+
     tasks.forEach((task,index)=>{
-        let li = document.createElement("li")
-        ul.appendChild(li)
-        li.innerHTML = `<div class="flex text-center  gap-4 mt-3 justify-between ">
-        <span class="text-center p-4 items-center">${index+1} - ${task.text}</span><span>
-        <button onclick="deleteTask(${index})" class="border bg-amber-500 hover:bg-lime-800 hover:text-white p-4 rounded-2xl">Delete</button>
-        </span>
-        </div>`
-    })
-    
+
+        let li = document.createElement("li");
+
+        li.innerHTML = `
+        <div class="flex gap-4 mt-3 justify-between">
+
+        <span>${index+1} - ${task.text}</span>
+
+        <div class="space-x-2">
+
+        <button onclick="editTask(${index})"
+        class="bg-blue-500 text-white p-2 rounded">
+        Edit
+        </button>
+
+        <button onclick="deleteTask(${index})"
+        class="bg-red-500 text-white p-2 rounded">
+        Delete
+        </button>
+
+        </div>
+
+        </div>
+        `;
+
+        ul.appendChild(li);
+
+    });
 }
+
 function addtask(){
-    let input = document.querySelector("#input")
-    let add = document.querySelector("#addbtn")
+
+    let input = document.querySelector("#input");
+    let add = document.querySelector("#addbtn");
+
     add.addEventListener("click",()=>{
-        if(input.value === " ") return
-        tasks.push({
-            text:input.value,
-            done:false
-        })
-       console.log(tasks);
-       input.value = ""
-       show() 
-    })
+
+        if(input.value.trim() === "") return;
+
+        if(editIndex === -1){
+
+            tasks.push({
+                text: input.value
+            });
+
+        }else{
+
+            tasks[editIndex].text = input.value;
+
+            editIndex = -1;
+        }
+
+        input.value = "";
+
+        show();
+
+    });
 }
+
 function deleteTask(index){
 
     tasks.splice(index,1);
@@ -36,4 +75,14 @@ function deleteTask(index){
 
 }
 
-addtask()
+function editTask(index){
+
+    let input = document.querySelector("#input");
+
+    input.value = tasks[index].text;
+
+    editIndex = index;
+
+}
+
+addtask();
