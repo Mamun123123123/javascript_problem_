37c90b2b13d60910a359d0c6d586e365b0091771
Mamun123123123 <almalm90459@gmail.com>
2026-03-15 01:57:07 +0600

    solve 2 job problem using loop

--- Problem1.js
+++ Problem1.js
@@ -42,12 +42,52 @@
 // console.log(result)
 
 
-let str = "Abdullah Al Mamun"
-//space onujayi alada hobe and array dibe
+// let str = "Abdullah Al Mamun"
+// //space onujayi alada hobe and array dibe
 
-let result = str.split(" ").map((item)=>item.split("").reverse().join(''))
-console.log(result.join(" "));
+// let result = str.split(" ").map((item)=>item.split("").reverse().join(''))
+// console.log(result.join(" "));
 
+
+// let arr = [1,2,3,4,5]
+// console.log(arr);
+// let r = arr.reverse()
+// console.log(arr);
+
+// console.log(r);
+
+// let re = []
+
+// for(let i=arr.length-1;i>=0;i--){
+//         re.push(arr[i])
+// }
+// console.log(arr);
+
+// console.log(re);
+
+
+// let arr = [1,2,3,4,5,6,7,8]
+// let result = []
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i] % 2 === 0){
+//         result.push(arr[i])
+//     }
+// }
+// console.log(result.reverse());
+// console.log(arr);
+
+
+
+let str = "aaabbc"
+let result = {}
+for(let i=0;i<str.length;i++){
+    if(result[str[i]]){
+        result[str[i]]++
+    }else{
+        result[str[i]] = 1
+    }
+}
+console.log(result);
 
 
 
