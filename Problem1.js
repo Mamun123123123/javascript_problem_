43c68c156d03ce64ba7c76bd7ee200502f542712
Mamun123123123 <almalm90459@gmail.com>
2026-03-14 19:40:41 +0600

    find exact letter in string using loop

--- Problem1.js
+++ Problem1.js
@@ -1,14 +1,24 @@
-let str = "Abdullah AL mamun"
-let result = str.split(' ')
-console.log(result);
- let max = result[0]
-for(let i=1;i<result.length;i++){
+// let result = str.split(' ')
+// console.log(result);
+// let max = result[0]
+// for(let i=1;i<result.length;i++){
    
-    if(max.length <result[i].length){
-        max = result[i]
-    }
+//     if(max.length <result[i].length){
+//         max = result[i]
+//     }
 
+// }
+// console.log(max);
+
+let str = "Abdullah al mamun"
+
+let result = []
+
+for(let i=0;i<str.length;i++){
+        if(str[i]+str[i+1] === "mu"){
+            result.push(str[i]+str[i+1])
+        }
 }
-console.log(max);
+console.log(result);
 
 
