02c3a787e06af334b0cba2be2ef920d3e456bd44
Mamun123123123 <almalm90459@gmail.com>
2026-03-16 01:42:49 +0600

    async await and createElement

--- image.js
+++ image.js
@@ -10,20 +10,25 @@
 
 
 let btn = document.querySelector("#btn")
-let h1 = document.querySelector("#h1")
+
 
 btn.addEventListener("click",async()=>{
      let response = await fetch("https://picsum.photos/v2/list?page=2&limit=100")
      let alldata = await response.json()
      console.log(alldata);
+     let show = document.querySelector("#show")
      alldata.forEach((item) =>{
         let div = document.createElement("div")
         let h1 = document.createElement("h1")
         let img = document.createElement("img")
-        
+        h1.textContent = item.author
+        img.src = item.download_url
+        div.className="border  m-4 flex flex-col item-center p-4 justify-center"
+        img.className = "w-80 object-cover rounded-full"
+        h1.className="font-bold"
         div.appendChild(img)
         div.appendChild(h1)
-
+        show.appendChild(div)
      })
 })
 
