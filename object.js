7e79db36109a34aca7b432830f3ca336d4a4857c
Mamun123123123 <almalm90459@gmail.com>
2026-03-16 13:00:27 +0600

    find object max value and string to object create

--- object.js
+++ object.js
@@ -44,3 +44,23 @@ for(let i=0;i<str.length;i++){
     }
 }
 console.log(result);
+
+let scores = {
+  math: 80,
+  english: 95,
+  science: 90
+}
+let results = []
+for (const key in scores) {
+    if(!results.includes(scores[key])){
+        results.push(scores[key])
+    }
+}
+console.log(results);
+let max = results[0]
+for(let i =0;i<results.length;i++){
+    if(max<results[i]){
+        max = results[i]
+    }
+}
+console.log(max);
