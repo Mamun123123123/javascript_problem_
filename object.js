2db713787a7f33cccf6305af4e4cd0f754062b10
Mamun123123123 <almalm90459@gmail.com>
2026-03-18 19:10:28 +0600

    create group  by age

--- object.js
+++ object.js
@@ -1,110 +1,132 @@
-// let user = {
-//   name: "Mamun",
-//   age: 22,
-//   city: "Dhaka"
-// }
-// let count = 0
-// let result = []
-// for (const key in user) {
-//     console.log(user[key]);
+// // let user = {
+// //   name: "Mamun",
+// //   age: 22,
+// //   city: "Dhaka"
+// // }
+// // let count = 0
+// // let result = []
+// // for (const key in user) {
+// //     console.log(user[key]);
 
-//  if(!result.includes(key)){
+// //  if(!result.includes(key)){
     
-//     result.push(key)
-//     count++
-//  }
+// //     result.push(key)
+// //     count++
+// //  }
     
-// }
-// console.log(count);
+// // }
+// // console.log(count);
 
+
+// // let obj = {
+// //   a: 10,
+// //   b: 20,
+// //   c: 30
+// // }
+
+
+// // let sum = 0
+// // for (const key in obj) {
+// //    sum = sum+obj[key]
+// // }
+// // console.log(sum);
+
+
+// // let str = "banana"
+// // let result = {}
+
+// // for(let i=0;i<str.length;i++){
+// //     if(result[str[i]]){
+// //         result[str[i]]++
+// //     }
+// //     else{
+// //         result[str[i]] = 1
+// //     }
+// // }
+// // console.log(result);
+
+// let scores = {
+//   math: 80,
+//   english: 95,
+//   science: 90
+// }
+// let results = []
+// for (const key in scores) {
+//     if(!results.includes(scores[key])){
+//         results.push(scores[key])
+//     }
+// }
+// console.log(results);
+// let max = results[0]
+// for(let i =0;i<results.length;i++){
+//     if(max<results[i]){
+//         max = results[i]
+//     }
+// }
+// console.log(max);
+
+// // let arr = ["apple","banana","mango"]
+// // let re = {}
+// // for(let i = 0 ;i<arr.length;i++){
+        
+// //     re[i] = arr[i]
+    
+// // }
+// // console.log(re);
+
+// let arr = ["apple","banana","apple","orange","banana"]
+// let res = {}
+// for(let i=0;i<arr.length;i++){
+//     if(res[arr[i]]){
+//         res[arr[i]]++
+//     }else{
+//         res[arr[i]] = 1
+//     }
+// }
+// console.log(res);
 
 // let obj = {
-//   a: 10,
-//   b: 20,
-//   c: 30
+//   name: "Mamun",
+//   city: "Dhaka"
 // }
 
-
-// let sum = 0
-// for (const key in obj) {
-//    sum = sum+obj[key]
-// }
-// console.log(sum);
-
-
-// let str = "banana"
 // let result = {}
 
-// for(let i=0;i<str.length;i++){
-//     if(result[str[i]]){
-//         result[str[i]]++
-//     }
-//     else{
-//         result[str[i]] = 1
+// for(let key in obj){
+//   result[obj[key]] = key
+// }
+
+// console.log(result)
+
+// let str = "hello"
+// let resultt = []
+// for(let i = 0;i<str.length;i++){
+//     if(resultt[str[i]]){
+//         resultt[str[i]]++
+//     }else{
+//         resultt[str[i]] = 1
 //     }
 // }
-// console.log(result);
+// console.log(resultt);
 
-let scores = {
-  math: 80,
-  english: 95,
-  science: 90
-}
-let results = []
-for (const key in scores) {
-    if(!results.includes(scores[key])){
-        results.push(scores[key])
-    }
-}
-console.log(results);
-let max = results[0]
-for(let i =0;i<results.length;i++){
-    if(max<results[i]){
-        max = results[i]
-    }
-}
-console.log(max);
 
-// let arr = ["apple","banana","mango"]
-// let re = {}
-// for(let i = 0 ;i<arr.length;i++){
-        
-//     re[i] = arr[i]
-    
-// }
-// console.log(re);
-
-let arr = ["apple","banana","apple","orange","banana"]
-let res = {}
-for(let i=0;i<arr.length;i++){
-    if(res[arr[i]]){
-        res[arr[i]]++
-    }else{
-        res[arr[i]] = 1
-    }
-}
-console.log(res);
-
-let obj = {
-  name: "Mamun",
-  city: "Dhaka"
-}
+let arr = [
+  {name:"A", age:20},
+  {name:"B", age:20},
+  {name:"C", age:25}
+]
 
 let result = {}
 
-for(let key in obj){
-  result[obj[key]] = key
+for(let i = 0; i < arr.length; i++){
+  let age = arr[i].age
+  let name = arr[i].name
+
+  if(result[age]){
+    result[age].push(name)
+  } else {
+    result[age] = [name]
+  }
 }
 
 console.log(result)
-
-let str = "hello"
-let resultt = []
-for(let i = 0;i<str.length;i++){
-    if(resultt[str[i]]){
-        resultt[str[i]]++
-    }else{
-        resultt[str[i]] = 1
-    }
-}
-console.log(resultt);
