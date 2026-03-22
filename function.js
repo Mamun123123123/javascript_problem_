a1a9c7fea83deb667ceaa3739ce366fcfd3010d7
Mamun123123123 <almalm90459@gmail.com>
2026-03-22 19:43:56 +0600

    How do you check if a given string is a palindrome?

--- function.js
+++ function.js
@@ -1,6 +1,23 @@
+  /// sum of array problem 1
 let arr = [1,2,3,4,5,6,7,8,9,10]
 function sum_Of_array(arr){
     // console.log(arr);
     return arr.reduce((item,sum)=>item+sum,0)
 }
 console.log(sum_Of_array(arr))
+
+////// check a given string is a palindrome or not
+
+// let str = "madam"
+function isPalindrome(str){
+    return str === str.split('').reverse().join('')
+}
+// console.log(isPalindrome(str));
+
+
+let str = ["madam","mamun","amma"]
+let result = []
+for(let i=0;i<str.length;i++){
+   result.push((isPalindrome(str[i])))
+} 
+console.log(result);
