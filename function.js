6cb7d1ece0dffc0b1cbbc433cf54fc64a41b89b8
Mamun123123123 <almalm90459@gmail.com>
2026-03-25 19:14:53 +0600

    check anagram

--- function.js
+++ function.js
@@ -76,4 +76,15 @@ for (let i = 0; i < arr.length; i++) {
 
 console.log(result) 
 
+function isAnagram(str1, str2) {
+  if (str1.length !== str2.length) return false
+
+  let sorted1 = str1.split('').sort().join('')
+  let sorted2 = str2.split('').sort().join('')
+
+  return sorted1 === sorted2
+}
+
+console.log(isAnagram("listen", "silent")) 
+
 
