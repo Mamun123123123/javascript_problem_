a327ce13f75a0133b67c6b2c6e315f28b84fea10
Mamun123123123 <almalm90459@gmail.com>
2026-03-26 21:12:06 +0600

    ascii code to char code

--- pattern.js
+++ pattern.js
@@ -1,8 +1,32 @@
 let prompt = require('prompt-sync')()
 let n = Number(prompt("Enter a number : "))
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=n;j++){
+//         process.stdout.write("*")
+//     }
+//     console.log();
+    
+// }
+
 for(let i=1;i<=n;i++){
-    for(let j=1;j<=n;j++){
+    for(let j=1;j<=i;j++){
+        process.stdout.write(j+" ")
+    }
+    console.log();
+    
+}
+for(let i=1;i<=n;i++){
+    for(let j=1;j<=i;j++){
         process.stdout.write("*")
+    }
+    console.log();
+    
+}
+for(let i=1;i<=n;i++){
+    let ascii = 65
+    for(let j=1;j<=i;j++){
+        process.stdout.write(String.fromCharCode(ascii)+" ")
+        ascii++
     }
     console.log();
     
