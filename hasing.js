7a8028f57e4ca856fb3fdd8ee46ee1c7574f9fc1
Mamun123123123 <almalm90459@gmail.com>
2026-03-27 19:36:22 +0600

    remove duplicate value using set function

--- hasing.js
+++ hasing.js
@@ -1,15 +1,23 @@
 let set = new Set()
-set.add(2)
-set.add(4)
-set.add(3)
-console.log(set);
-console.log(set.has(2));
-console.log(set.size);
-set.delete(2)
-console.log(set);
+// set.add(2)
+// set.add(4)
+// set.add(3)
+// console.log(set);
+// console.log(set.has(2));
+// console.log(set.size);
+// set.delete(2)
+// console.log(set);
 
-for (const a of set) {
-         console.log(a);
+// for (const a of set) {
+//          console.log(a);
              
-}
+// }
 
+
+let arr = [1,2,4,1,2,4,3,1,7,7]
+for(let i=0;i<arr.length;i++){
+        if(!set.has(arr[i])){
+               set.add(arr[i])
+        }
+}
+console.log(set);
