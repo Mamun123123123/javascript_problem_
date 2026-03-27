18b9537b8c27536a4e9166ca80241bb6fe645193
Mamun123123123 <almalm90459@gmail.com>
2026-03-27 20:28:21 +0600

    count element using map

--- hasing.js
+++ hasing.js
@@ -22,13 +22,47 @@ let arr = [1,2,4,1,2,4,3,3,1,7,7]
 // }
 // console.log(set);
 
+// for(let i=0;i<arr.length;i++){
+//     if(set.has(arr[i])){
+//         set.delete(arr[i])
+//     }else{
+//         set.add(arr[i])
+//     }
+// }
+// console.log(set);
+
+// let str = "mamun"
+
+// for(let i =0;i<str.length;i++){
+//     if(!set.has(str[i])){
+//         set.add(str[i])
+//     }
+// }
+// console.log(set);
+
+
+
+let map = new Map()
+
+// map.set("name","mamun")
+//  console.log(map);
+ 
 for(let i=0;i<arr.length;i++){
-    if(set.has(arr[i])){
-        set.delete(arr[i])
+    if(map.has(arr[i])){
+        map.set(arr[i],map.get(arr[i]) + 1)
     }else{
-        set.add(arr[i])
+        map.set(arr[i],1)
     }
 }
-console.log(set);
+console.log(map);
 
+let result = {}
+for(let i =0 ;i<arr.length;i++){
+    if(result[arr[i]]){
+        result[arr[i]]++
+    }else{
+        result[arr[i]] = 1
+    }
+}
+console.log(result);
 
