92f678b7ea00c78c086c61019272766ee7cacabe
Mamun123123123 <almalm90459@gmail.com>
2026-03-27 17:12:02 +0600

    bubble sort

---
+++ bubble.js
@@ -0,0 +1,12 @@
+let arr = [30,1,40,10,4,50]
+let n = arr.length
+for(let i=0;i<n-1;i++){
+    for(let j=0;j<n-1-i;j++){
+        if(arr[j]>arr[j+1]){
+            let temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+        }
+    }
+}
+console.log(arr);
