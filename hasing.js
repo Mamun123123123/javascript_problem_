2990f5a479c3fe34133cf49a117a5601b25d74eb
Mamun123123123 <almalm90459@gmail.com>
2026-03-29 20:42:19 +0600

    complete hashing

--- hasing.js
+++ hasing.js
@@ -48,11 +48,12 @@ let map = new Map()
 //  console.log(map);
  
 for(let i=0;i<arr.length;i++){
-    if(map.has(arr[i])){
-        map.set(arr[i],map.get(arr[i]) + 1)
-    }else{
-        map.set(arr[i],1)
-    }
+    // if(map.has(arr[i])){
+    //     map.set(arr[i],map.get(arr[i]) + 1)
+    // }else{
+    //     map.set(arr[i],1)
+    // }
+    map.set(arr[i],(map.get(arr[i]),0) + 1)
 }
 console.log(map);
 
