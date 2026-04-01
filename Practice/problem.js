fa0e1ad1a608ce47690bcbdf374f4eadf7935ca6
Mamun123123123 <almalm90459@gmail.com>
2026-04-01 19:43:47 +0600

    count vowels and find vowels

--- Practice/problem.js
+++ Practice/problem.js
@@ -1,15 +1,38 @@
-let str = "mamun"
-let result = []
-for(let i=0;i<str.length;i++){
-        result.unshift(str[i])
-}
-console.log(result.join(''));
+// let str = "mamun"
+// let result = []
+// for(let i=0;i<str.length;i++){
+//         result.unshift(str[i])
+// }
+// console.log(result.join(''));
 
-let arr = [1,5,9,2]
-let max = arr[0]
+// let arr = [1,5,9,2]
+// let max = arr[0]
+// for(let i=0;i<arr.length;i++){
+//     if(arr[i]>max){
+//         max = arr[i]
+//     }
+// }
+// console.log(max);
+
+
+let arr = [2,1,1,4,0,2,6,8]
+let removeDuplicate = []
 for(let i=0;i<arr.length;i++){
-    if(arr[i]>max){
-        max = arr[i]
+    if(!removeDuplicate.includes(arr[i])){
+             removeDuplicate.push(arr[i])
     }
 }
-console.log(max);
+console.log(removeDuplicate);
+
+let str = "mamun"
+let vowels = "aeiouAEIOU"
+let result = ""
+for(let i=0;i<str.length;i++){
+    if(vowels.includes(str[i])){
+        result += str[i]
+    }
+} 
+console.log(result);
+console.log(result.length);
+
+
